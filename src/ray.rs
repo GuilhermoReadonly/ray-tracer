6d20d2f2370a95564a004e19f525a2d7e50d550e
guilhermo <guilhermo.readonly@gmail.com>
2020-09-13 15:35:12 +0200

    6.2 Simplifying the Ray-Sphere Intersection Code subsection

--- src/ray.rs
+++ src/ray.rs
@@ -19,6 +19,7 @@ impl Ray {
         match has_hit {
             Some(t) => {
                 let n: Vec3 = Vec3::unit(self.at(t) - Vec3::new(0.0, 0.0, -1.0));
+                // we map each component of the normal vector from [-1, 1] to [0, 1] so it can fit in a Color vector
                 Color::new_with_vec(0.5 * Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0))
             }
             None => {
@@ -33,14 +34,35 @@ impl Ray {
 
     fn hit_sphere(&self, center: Vec3, radius: f64) -> Option<f64> {
         let oc: Vec3 = self.origin - center;
-        let a = Vec3::dot(&self.direction, &self.direction);
-        let b = 2.0 * Vec3::dot(&oc, &self.direction);
-        let c = Vec3::dot(&oc, &oc) - radius * radius;
-        let discriminant = b * b - 4.0 * a * c;
+        let a = self.direction.length_squared();
+        let half_b = Vec3::dot(&oc, &self.direction);
+        let c = oc.length_squared() - radius.powi(2);
+        let discriminant = half_b.powi(2) - a * c;
         if discriminant < 0.0 {
             None
         } else {
-            Some((-b - discriminant.sqrt()) / (2.0 * a))
+            Some((-half_b - discriminant.sqrt()) / a)
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //use rand::prelude::*;
+    use test;
+
+    #[test]
+    fn hit_sphere_test() {
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let direction = Vec3::new(0.0, 0.0, -1.0);
+        let ray = Ray::new(origin, direction);
+        let sphere_center = Vec3::new(0.0, 0.0, 0.0);
+        let sphere_radius = 0.5;
+
+        let result = ray.hit_sphere(sphere_center, sphere_radius);
+
+        assert_eq!(result, Some(-0.5))
     }
 }
