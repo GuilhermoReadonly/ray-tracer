d3657432fd4f6af1d37fa5b7e5608bc3a19b8740
Guilhem Radonde <gradonde@slb.com>
2024-01-24 19:19:55 +0100

    init

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,50 @@ pub use materials::*;
 pub use ray::*;
 pub use world::*;
 
+
+
+
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
+}
+
+
+
+
+
+
+
+
+
 fn clamp(x: f64, min: f64, max: f64) -> f64 {
     if x < min {
         min
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
-use ray_tracer::{self, RTError};
+use ray_tracer::{self, RTError, run};
 use std::time::Instant;
 mod scenes;
 
 fn main() -> Result<(), RTError> {
     println!("Starting...");
-
-    // Create scene, empty image and other parameters
-    let (img, world, camera, samples_per_pixel, depth) = scenes::random_scene_with_lights();
-
-    // Render Image
     let now = Instant::now();
-    let img = ray_tracer::create_img(img, world, samples_per_pixel, camera, depth);
-    let gen_time = now.elapsed().as_secs_f64();
-    println!("Image generated in {} s", gen_time);
 
-    // Write to file
-    let now = Instant::now();
-    ray_tracer::write_img_to_file("./target/img.jpg", &img)?;
-    // Archive with parameters in file name
-    ray_tracer::write_img_to_file(
-        &format!(
-            "./target/img-size_{}x{}-depth_{}-samples_{}-aperture_{}-focus_{}-fov_{:.2}-time_{:.3}.jpg",
-            img.width, img.height, depth, samples_per_pixel, camera.aperture, camera.focus_dist, camera.vfov, gen_time
-        ),
-        &img,
-    )?;
-    println!("Image written in {} s", now.elapsed().as_secs_f64());
+    run();
+
+    println!("Run in {} s", now.elapsed().as_secs_f64());
+
+    // // Create scene, empty image and other parameters
+    // let (img, world, camera, samples_per_pixel, depth) = scenes::random_scene_with_lights();
+
+    // // Render Image
+    // let now = Instant::now();
+    // let img = ray_tracer::create_img(img, world, samples_per_pixel, camera, depth);
+    // let gen_time = now.elapsed().as_secs_f64();
+    // println!("Image generated in {} s", gen_time);
+
+    // // Write to file
+    // let now = Instant::now();
+    // ray_tracer::write_img_to_file("./target/img.jpg", &img)?;
+    // // Archive with parameters in file name
+    // ray_tracer::write_img_to_file(
+    //     &format!(
+    //         "./target/img-size_{}x{}-depth_{}-samples_{}-aperture_{}-focus_{}-fov_{:.2}-time_{:.3}.jpg",
+    //         img.width, img.height, depth, samples_per_pixel, camera.aperture, camera.focus_dist, camera.vfov, gen_time
+    //     ),
+    //     &img,
+    // )?;
+    // println!("Image written in {} s", now.elapsed().as_secs_f64());
 
     Ok(())
 }
